92a677803ec5b94e8bb5a7c2435357e25ed78615
Sávio Henrique <araujosavio99@gmail.com>
2019-09-02 15:12:41 -0300

    Update Cadastro

--- Hotel/src/br/com/menu/Cadastro.java
+++ Hotel/src/br/com/menu/Cadastro.java
@@ -3,7 +3,6 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.text.ParseException;
-//import java.text.SimpleDateFormat;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -11,7 +10,7 @@ import br.com.clientes.Consumo;
 import br.com.clientes.Conta;
 import br.com.clientes.Hospede;
 import br.com.hotel.Aposento;
-import br.com.hotel.Hospedagem;
+import br.com.hotel.Reserva;
 import br.com.hotel.Hotel;
 /**
  * 
@@ -24,30 +23,30 @@ public class Cadastro {
 	 * 
 	 * @param aposentos_disponiveis
 	 * @param hotel
-	 * @param hospedagens
+	 * @param reservas
 	 * @return
 	 * @throws ParseException
 	 */
-	public static int cadastro(ArrayList <Aposento> aposentos_disponiveis, Hotel hotel, ArrayList <Hospedagem> hospedagens) throws ParseException {
+	public static int cadastro(ArrayList <Aposento> aposentos_disponiveis, Hotel hotel, ArrayList <Reserva> reservas) throws ParseException {
+		ler = new Scanner(System.in);
 		double valor_conta = 0;
 		boolean pago_conta = false, responsavel = false;
-		ler = new Scanner(System.in);
+		boolean check = true;
 		Random rnd = new Random();
 		int codigo = rnd.nextInt(100)+1;  //Codigo unico de cada hospede
-		boolean check = true;
-		String resposta;
-		Date dataEntrada, dataSaida;
 		int codigo_hospede, codigo_conta, opcao;
-		String nome, CPF, RG, telefone;
+		Date dataEntrada, dataSaida;
+		String nome, CPF, RG, telefone, resposta;
+		
 		
 		System.out.println("Deseja se hospedar no " + hotel.getNome() + " ? s/n ");
 		resposta = ler.nextLine();
 		if(resposta.contentEquals("s")) {
-			check = Hospedagem.checagemDeCodigo(codigo, hospedagens);
-			while(!check) {
-				check = Hospedagem.checagemDeCodigo(codigo, hospedagens);
+			do{
+				check = Reserva.checagemDeCodigo(codigo, reservas);
 				codigo = rnd.nextInt(100)+1;
-			}
+			}while(!check);
+			
 			String dataRecebida;
 			System.out.println("Insira a data de entrada(dd/mm/yyyy): ");
 			dataRecebida = ler.nextLine();
@@ -85,7 +84,6 @@ public class Cadastro {
 				int j = i+1;
 				System.out.println("Aposento " + j + ": ");
 				System.out.println("Desricao: " + aposentos_disponiveis.get(i).getDescricao());
-				System.out.println("Capacidade: " + aposentos_disponiveis.get(i).getCapacidade());
 				System.out.println("Numero: " + aposentos_disponiveis.get(i).getNumero());
 				System.out.println("Valor da diaria: R$" + aposentos_disponiveis.get(i).getValor());
 				System.out.println();
@@ -97,18 +95,42 @@ public class Cadastro {
 			Aposento aposento_escolhido = new Aposento();
 			aposento_escolhido = aposentos_disponiveis.get(opcao-1);
 			
+			int controle = 0;
+			int quantidadeReservas = 0;
+			
+			for(int i=0; i < reservas.size(); i++) {
+				if(reservas.get(i).getAposento().getCodigo() == opcao-1) {
+					quantidadeReservas++;
+					//Compara se a data de entrada e saída são menores que a data de entrada
+					if(dataEntrada.compareTo(reservas.get(i).getDataEntrada()) < 0) {
+						if(dataSaida.compareTo(reservas.get(i).getDataEntrada()) < 0) {
+							controle++;
+						}
+					}
+					//Compara se a data de entrada e saída são maiores que a data de saída
+					if(dataEntrada.compareTo(reservas.get(i).getDataSaida()) > 0) {
+						controle++;
+					}	
+				}
+			}
+			
+			if(controle != quantidadeReservas) {
+				System.out.println("O  aposento não esta disponível para esse período!");
+				return -1;
+			}
 			
 			//Remove o aposento escolhido da lista de aposentos disponiveis
 			//aposentos_disponiveis.remove(opcao - 1); 
 			
+			//Vai criar uma conta para cada hospede ou só para o hospede responsável?
 			codigo_conta = codigo;
 			
 			ArrayList<Consumo> consumos = new ArrayList<Consumo>();
 			
 			Conta conta = new Conta(codigo_conta, valor_conta, pago_conta, consumos);
 			
-			Hospedagem hospedagem = new Hospedagem(codigo, dataEntrada, dataSaida, hospede, aposento_escolhido, conta);
-			hospedagens.add(hospedagem);		
+			Reserva reserva = new Reserva(codigo, dataEntrada, dataSaida, hospede, aposento_escolhido, conta);
+			reservas.add(reserva);		
 		}
 		return codigo;
 	}
